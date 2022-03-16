bb48a9ce9f450873205da3e4d8285dc5394ec478
shshwdr <cs.vivian04@gmail.com>
2022-03-16 00:25:42 -0700

    add level 234 to test bird and mouse

--- Assets/ClickToAttach.cs
+++ Assets/ClickToAttach.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class ClickToAttach : MonoBehaviour
 {
+    Rigidbody2D rb;
+    public bool isAttached = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        updateAttach();
+    }
+
+    private void OnMouseDown()
+    {
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (GetComponent<NPC>().isLinked)
+            {
+                isAttached = !isAttached;
+                updateAttach();
+            }
+            
+        }
+    }
+
+    void updateAttach()
+    {
+        if (isAttached)
+        {
+
+            rb.bodyType = RigidbodyType2D.Static;
+        }
+        else
+        {
+            rb.bodyType = RigidbodyType2D.Dynamic;
+        }
     }
 }
